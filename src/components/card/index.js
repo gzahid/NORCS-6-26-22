f0fba30df98f68995e90c66d4c9f1e15d91c6cc6
ghaniz <ghanizahid@hotmail.com>
2022-07-26 19:39:15 -0400

    cards generation and css

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,3 +1,3 @@
-export default function Card(){
-    return <>Card</>;
+export default function Card({cardValue}){
+    return <div className="card" style={{height:"120px", width:"80px", border:"1px solid black"}}>{cardValue}</div>;
 }
--- src/components/cardContainer/index.js
+++ src/components/cardContainer/index.js
@@ -1,12 +1,14 @@
 import Card from  "../card";
 
-export default function CardContainer({randomNum, name}){
-    console.log(randomNum);
-    return(
-        <>
-            {randomNum}
-            {name}
-            <Card />
-        </>
-    );
+export default function CardContainer({ cards }){
+    console.log("test card", cards);
+    const cardsProcessed = cards.map((card,index) => {
+        return(
+            <div key={index}>
+                <Card cardValue={card} />
+            </div>
+        );
+    });
+    return <div>{cardsProcessed}</div>;
+    
 }
