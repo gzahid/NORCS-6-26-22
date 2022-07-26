a4f8af332b84f5ea3f50eb983bd2049246e8f7c4
ghaniz <ghanizahid@hotmail.com>
2022-07-26 18:52:39 -0400

    intial card setup

---
+++ src/components/buttonContainer/index.js
@@ -0,0 +1,8 @@
+export default function ButtonContainer(){
+    return (
+        <div>
+            <h1>ButtonContainer</h1>
+            <button>Make Card</button>
+        </div>
+    );
+}
---
+++ src/components/cardContainer/index.js
@@ -0,0 +1,9 @@
+import Card from  "../card";
+
+export default function CardContainer(){
+    return(
+        <>
+            <Card />
+        </>
+    );
+}
